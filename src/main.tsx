f35b7ace235f0bbff2807996ae19235684319414
Hunter S <67876717+hunterschep@users.noreply.github.com>
2025-02-13 00:24:04 -0500

    add custom coloring and tailwind global rules

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Home, About, Join } from "./pages";
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {
       colors: {
-        'primary-green': '#98002E',
-        'secondary-teal': '#C34B4B',
-        'primary-yellow': '#FFD166',
-        'background-black': '#FFEDDF',
-        'font-gray': '#0D1321',
       },
     },
   },
   plugins: [],
 }
-
