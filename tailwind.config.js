9747ab61bc8c037df04f7acb64f504a4d4a19420
Hunter S <67876717+hunterschep@users.noreply.github.com>
2025-02-12 23:54:28 -0500

    add color themes in tailwind.config.js
    Needs to be on main

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary-green': '#98002E',
+        'secondary-teal': '#C34B4B',
+        'primary-yellow': '#FFD166',
+        'background-black': '#FFEDDF',
+        'font-gray': '#0D1321',
+      },
+    },
+  },
+  plugins: [],
+}
+
