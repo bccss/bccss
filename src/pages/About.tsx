d02c89ee063bc1278a6a34332c36868935b689ad
taliakusmirek <kusmire@bc.edu>
2025-02-22 20:29:03 -0500

    updated changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,87 +1,82 @@
-/* https://heroicons.com/ for icons! */
-
-import viteLogo from '/vite.svg';
+import about_club_photo from '/about_club_photo.jpg';
 import '../index.css';
+import boardMembers from '../../board_members.json';
+import { ImageCarousel } from "../../src/components/ImageCarousel";
 
-const eBoardMembers = [
-    { name: "e", title: "e", linkedin: "e" },
-    { name: "e", title: "e", linkedin: "e" },
-    { name: "e", title: "e", linkedin: "e" },
-  ];
+type BoardMember = {
+    name: string;
+    title: string;
+    linkedin: string;
+    image: string;
+};
+const eBoardMembers: BoardMember[] = boardMembers;
 
 export const About = () => {
   return (
-        <div className="bg-backgroundBlack text-fontGray w-full min-h-screen mx-auto">
+    <div className="bg-backgroundBlack text-fontGray w-full min-h-screen mx-auto px-4 sm:px-8">
         {/* Navigation here */}
 
         {/* About */}
-        <div className="max-w-md mx-auto py-8 px-4 text-center">
-        <h2 className="text-2xl font-bold mb-4 text-primaryGreen flex items-center justify-left">            
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 w-8 h-8 mr-4 text-primaryGreen">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
-        </svg>
-            Our Mission
-        </h2>
-        <div className="flex flex-row items-start space-x-8"> 
-            <div className="flex flex-col">
-            <p className="text-font-gray mb-6 font-inter text-left">
-                The Boston College Computer Science Society (BCCSS) is dedicated to helping students interested in technology and entrepreneurship to achieve their goals. Throughout the school year we host events such as guest lectures, workshops, hackathons, collaborative projects, and more.
-            </p>
-            <p className="text-font-gray mb-6 font-inter text-left">
-                We work closely with the Computer Science department at BC to create a better environment for students to learn and find new opportunities. Further, we collaborate with many departments and clubs in the Carroll School of Management at BC to encourage entrepreneurship and promote collaboration between business and computer science students.
-            </p>
+        <div className="mx-auto py-8 text-left max-w-screen-lg">
+            <h2 className="text-2xl font-bold mb-4 pt-8 text-primaryGreen flex items-center">            
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-8 h-8 mr-4 text-primaryGreen">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>
+                Our Mission
+            </h2>
+            <div className="flex flex-col md:flex-row md:space-x-8 items-center">
+                <div className="flex-1">
+                    <p className="text-font-gray mb-6 mt-4 font-inter">
+                        The Boston College Computer Science Society (BCCSS) is dedicated to helping students interested in technology and entrepreneurship to achieve their goals. We host events such as guest lectures, workshops, hackathons, collaborative projects, and more.
+                    </p>
+                    <p className="text-font-gray mb-6 font-inter">
+                        We work closely with the Computer Science department at BC to create a better environment for students to learn and find new opportunities. Further, we collaborate with many departments and clubs in the Carroll School of Management at BC to encourage entrepreneurship.
+                    </p>
+                </div>
+                <img src={about_club_photo} className="w-full md:w-1/2 lg:w-1/3 h-auto border-3 border-dashed rounded-lg border-teal-300 bg-transparent mt-4" alt="BCCSS Club Photo" />
             </div>
-        <div className="bg-gray-400 w-[50%] h-[100%] flex items-center justify-center rounded-lg">
-            <img src={viteLogo} className="about_club_photo" alt="BCCSS Club Photo for About Page" />
-        </div>
-        </div>
         </div>
 
-        
         {/* Club Stats */}
-        <div className="flex justify-center gap-8 my-8">
-            <div className="group p-4 rounded-lg text-center w-48 text-secondaryTeal 
-            border-2 border-teal-300 bg-transparent
-            hover:bg-radial-[at_50%_75%] hover:from-teal-200 hover:via-teal-300 hover:to-teal-500
-           hover:border-teal-400 hover:shadow-2xl hover:translate-y-[-8px] hover:text-backgroundBlack transition-all duration-400 ease-in-out">
-                <h4 className="text-3xl font-bold">1000+</h4>
-                <p className="text-xl font-inter">members</p>
+        <div className="flex flex-wrap justify-center gap-8 my-8">
+            <div className="group p-4 rounded-lg text-center w-40 md:w-48 text-secondaryTeal border-2 border-teal-300 bg-transparent hover:bg-teal-500 hover:border-teal-400 hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">
+                <h4 className="text-3xl font-bold group-hover:!text-black">1000+</h4>
+                <p className="text-xl font-inter group-hover:!text-black">members</p>
+            </div>
+            <div className="group p-4 rounded-lg text-center w-40 md:w-48 text-secondaryTeal border-2 border-teal-300 bg-transparent hover:bg-teal-500 hover:border-teal-400 hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">
+                <h4 className="text-3xl font-bold group-hover:!text-black">34</h4>
+                <p className="text-xl font-inter group-hover:!text-black">events hosted</p>
+            </div>
         </div>
-        <div className="group p-4 rounded-lg text-center w-48 text-secondaryTeal 
-            border-2 border-teal-300 bg-transparent
-            hover:bg-radial-[at_50%_75%] hover:from-teal-200 hover:via-teal-300 hover:to-teal-500
-          hover:border-teal-400 hover:shadow-2xl hover:translate-y-[-8px] hover:text-backgroundBlack transition-all duration-400 ease-in-out">
-                <h4 className="text-3xl font-bold">34</h4>
-                <p className="text-xl font-inter">events hosted</p>
-        </div>
+
+        {/* Image Carousel */}
+        <div className="my-24 max-w-screen-lg mx-auto">
+            <ImageCarousel />
         </div>
 
         {/* EBoard */}
-        <div className="max-w-md mx-auto py-20 px-4">
-        <h2 className="text-2xl font-bold mb-4 text-primaryGreen flex items-center justify-center">            
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 w-8 h-8 mr-2 color-primaryGreen">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-            </svg>
-            Meet the Board
+        <div className="max-w-screen-lg mx-auto py-20 px-4">
+            <h2 className="text-2xl font-bold mb-4 text-primaryGreen flex items-center justify-center">            
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-8 h-8 mr-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
+                Meet the Board
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 mt-12 rounded-lg">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
                 {eBoardMembers.map((member, index) => (
-                <div key={index} className="flex flex-col items-center justify-center text-center">
-                    <div className="w-32 h-32 bg-gray-300 rounded-lg overflow-hidden mb-4">
-                        <img src={viteLogo} className="w-full h-full object-cover" alt="BCCSS Eboard PFP" />
+                    <div key={index} className="flex flex-col items-center text-center p-4 bg-gray-800 rounded-lg shadow-lg min-h-[250px]">
+                        <div className="w-28 h-28 bg-gray-300 rounded-full overflow-hidden mb-4">
+                            <img src={member.image} className="w-full h-full object-cover" alt="Board Member" />
+                        </div>
+                        <h4 className="text-lg font-semibold text-primaryGreen min-h-[24px]">{member.name}</h4>
+                        <p className="text-sm text-secondaryTeal font-inter min-h-[40px]">{member.title}</p>
+                        <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm mt-2">
+                            LinkedIn
+                        </a>
                     </div>
-                <h4 className="text-lg font-montserrat text-primaryGreen">{member.name}</h4>
-                <p className="text-sm text-secondaryTeal font-inter">{member.title}</p>
-                <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm mt-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0,0,256,256">
-                    <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(5.33333,5.33333)"><path d="M42,37c0,2.762 -2.238,5 -5,5h-26c-2.761,0 -5,-2.238 -5,-5v-26c0,-2.762 2.239,-5 5,-5h26c2.762,0 5,2.238 5,5z" fill="#35d2a1"></path><path d="M12,19h5v17h-5zM14.485,17h-0.028c-1.492,0 -2.457,-1.112 -2.457,-2.501c0,-1.419 0.995,-2.499 2.514,-2.499c1.521,0 2.458,1.08 2.486,2.499c0,1.388 -0.965,2.501 -2.515,2.501zM36,36h-5v-9.099c0,-2.198 -1.225,-3.698 -3.192,-3.698c-1.501,0 -2.313,1.012 -2.707,1.99c-0.144,0.35 -0.101,1.318 -0.101,1.807v9h-5v-17h5v2.616c0.721,-1.116 1.85,-2.616 4.738,-2.616c3.578,0 6.261,2.25 6.261,7.274l0.001,9.726z" fill="#ffffff"></path></g></g>
-                    </svg>
-                </a>
-                </div>
                 ))}
             </div>
-      </div>
-        {/* Footer Component here \*/}
+        </div>
     </div>
-  )
+  );
 };
